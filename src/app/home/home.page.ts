29803ecc772fd5fd46ce10c7ba54b97d3d620d70
Seu Nome <seuemail@example.com>
2025-03-07 20:14:47 -0300

    Radio e Alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
-})
-export class HomePage {
+  @Component({
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: false,
+  })
+  export class HomePage {
 
-  constructor() {}
+    valorCompra = 0;
+    desconto = '';
+    descontoEscolhido= '';
+    alertButtons = ['OK'];
 
+    constructor() {}
+
+    calculoDesconto(){
+
+      let valorDesconto = 0;
+      let valorFinal = 0;
+
+      if(this.descontoEscolhido == 'creditoVista'){
+        valorDesconto == this.valorCompra*0.05;
+        valorFinal == this.valorCompra - valorDesconto;
+        this.descontoEscolhido = 'Desconto de R${valorDesconto} aplicado, Valor final: R${valorFinal}'
+      }
+  }
 }
